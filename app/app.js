d68c8ee401a3e11898d4a73aec382de87d315ebb
Adham Khatean <aekhatean@gmail.com>
2021-12-17 21:39:38 +0200

    Add content to sections welcome, about me, and services

--- app/app.js
+++ app/app.js
@@ -1,12 +1,27 @@
 $(document).ready(function () {
-  console.log($("#fullpage"));
+  // Create the full page scrolling effect
   $("#fullpage").fullpage({
     //options here
     autoScrolling: true,
-    scrollHorizontally: true,
     navigation: true,
-    fitToSection: true,
-    fitToSectionDelay: 1000,
-    slidesNavPosition: "bottom",
+    paddingTop: "3.5rem",
+    navigationTooltips: [
+      "Welcome",
+      "About me",
+      "My services",
+      "My skills",
+      "Study cases",
+      "Contact me",
+    ],
+  });
+
+  // Create smooth scrolling effect on scroll (Cannot solve it with jQuery *Think of a jQuery solution later*)
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
   });
 });
